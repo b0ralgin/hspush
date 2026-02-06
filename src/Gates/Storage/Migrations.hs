c20dc2dc6741be267139732129dda72c16bd9edf
Kirill <kirill.borodylin@gmail.com>
2026-02-06 14:46:36 +0200

    add migrations

--- src/Gates/Storage/Migrations.hs
+++ src/Gates/Storage/Migrations.hs
@@ -9,7 +9,14 @@ migrations = []
 
 
 migrateUp :: MigrationName -> SQLite ()
-migrateUp "add_devices" = void (run "CREATE TABLE devices ()")
+migrateUp "add_devices" = void (run "CREATE TABLE IF NOT EXISTS  devices (user_id TEXT, device_id TEXT, platform TEXT,  PRIMARY KEY (user_id, device_id))")
+migrateUp "add_tasks" = void (run "CREATE TABLE IF NOT EXISTS  tasks ( \
+  \ id INTEGER PRIMARY KEY AUTOINCREMENT,  -- или просто INTEGER PRIMARY KEY \
+ \ device_id TEXT NOT NULL, \
+  \ title TEXT NOT NULL, \
+  \ body TEXT NOT NULL)")
 
 migrateDown :: MigrationName -> SQLite ()
-migrateDown "add_devices" = void (run "DROP TABLE devices")
+migrateDown "add_devices" = void (run "DROP TABLE IF EXISTS  devices")
+migrateDown "add_tasks" = void (run "DROP TABLE IF  EXISTS tasks")
+  
